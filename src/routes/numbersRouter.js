bd10d35e818cec2640eae7fd66d9a3b94de4fcd8
Justin Sandahl <sandahl.j.d@gmail.com>
2020-04-26 14:36:49 +0100

    finished adding numbersRoutes for the remaining numbers.tests, now handles requests made to the remainder functions.

--- src/routes/numbersRouter.js
+++ src/routes/numbersRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { add, subtract, multiply, divide } = require('../lib/numbers');
+const { add, subtract, multiply, divide, remainder } = require('../lib/numbers');
 
 const numbersRouter = express.Router();
 
@@ -35,11 +35,23 @@ numbersRouter.post('/divide', (req, res) => {
   if (req.body.b === 0) {
     res.status(400).json({ error: 'Unable to divide by 0.' });
   } else if (req.body.a === undefined || req.body.b === undefined) {
-    res.status(400).json({error: 'Parameters "a" and "b" are required.' });
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
   } else if (Number.isNaN(parseInt(req.body.a)) || Number.isNaN(parseInt(req.body.b))) {
     res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
   } else {
     res.status(200).json({ result: divide(req.body.a, req.body.b) });
+  }
+});
+
+numbersRouter.post('/remainder', (req, res) => {
+  if (req.body.b === 0) {
+    res.status(400).json({ error: 'Unable to divide by 0.' });
+  } else if (req.body.a === undefined || req.body.b === undefined) {
+    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
+  } else if (Number.isNaN(parseInt(req.body.a)) || Number.isNaN(parseInt(req.body.b))) {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  } else {
+    res.status(200).json({ result: remainder(req.body.a, req.body.b) });
   }
 });
 
