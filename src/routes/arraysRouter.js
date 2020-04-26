516ca360ae39d5a0231082f7ec75d9bc62740f84
Justin Sandahl <sandahl.j.d@gmail.com>
2020-04-26 18:36:31 +0100

    refactored the arraysRouter and added the arraysController.js file that now contains the logic for handle each of the routed arrays requests.

--- src/routes/arraysRouter.js
+++ src/routes/arraysRouter.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const { getNthElement, arrayToCSVString, addToArray2, elementsStartingWithAVowel, removeNthElement2 } = require('../lib/arrays');
+const arraysController = require('../controllers/arraysController');
 
 const router = express.Router();
 
-router.post('/element-at-index/:index', (req, res) => {
-  res.status(200).json({ result: getNthElement(req.params.index, req.body.array) });
-});
+router.post('/element-at-index/:index', arraysController.arrays_getNthElement); 
 
-router.post('/to-string', (req, res) => {
-  res.status(200).json({ result: arrayToCSVString(req.body.array) });
-});
+router.post('/to-string', arraysController.arrays_arrayToCSVString);
 
-router.post('/append', (req, res) => {
-  res.status(200).json({ result: addToArray2(req.body.value, req.body.array) });
-});
+router.post('/append', arraysController.arrays_addToArray2);
 
-router.post('/starts-with-vowel', (req, res) => {
-  res.status(200).json({ result: elementsStartingWithAVowel(req.body.array) });
-});
+router.post('/starts-with-vowel', arraysController.arrays_elementsStartingWithAVowel);
 
-router.post('/remove-element', (req, res) => {
-  let { index } = req.query || 0;
-  const { array } = req.body;
-
-  res.status(200).json({ result: removeNthElement2(index, array) });
-});
+router.post('/remove-element', arraysController.arrays_removeNthElement2);
 
 module.exports = router;
